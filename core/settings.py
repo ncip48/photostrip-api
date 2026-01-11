adab806f9a274d005b87ba278b6778140b95fc47
herlyplanviu <herly@planviu.com>
2026-01-11 20:54:23 +0700

    plenger

--- core/settings.py
+++ core/settings.py
@@ -208,5 +208,12 @@ INTERNAL_IPS = [
     "127.0.0.1",
 ]
 
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    "https://photostrip.batakscript.id",
+    "http://localhost:5173",
+]
+
 CORS_ALLOW_CREDENTIALS = True
+
+# VERY IMPORTANT
+CORS_ALLOW_ALL_ORIGINS = False
