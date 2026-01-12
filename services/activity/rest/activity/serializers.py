4707b599f9e8701c62e6fc8b5ee21267edc322a4
herlyplanviu <herly@planviu.com>
2026-01-12 13:50:23 +0700

    a

--- services/activity/rest/activity/serializers.py
+++ services/activity/rest/activity/serializers.py
@@ -1,9 +1,12 @@
 # serializers.py
+from services.account.rest.user.serializers import ProfileSerializer
 from core.common.serializers import BaseModelSerializer
 from rest_framework import serializers
 from services.activity.models import Activity
 
 class ActivitySerializer(BaseModelSerializer):
+    user = ProfileSerializer()
+    
     class Meta:
         model = Activity
         fields = [
