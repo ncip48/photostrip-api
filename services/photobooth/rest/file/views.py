fe0f0d848b17d50017eeb5d28b00d59d9a127f83
herlyplanviu <herly@planviu.com>
2026-03-25 15:18:31 +0700

    ref

--- services/photobooth/rest/file/views.py
+++ services/photobooth/rest/file/views.py
@@ -2,9 +2,6 @@ from __future__ import annotations
 from services.photostrip.utils.generator import generate_photostrip
 from services.photostrip.rest.generate.serializers import GeneratePhotostripSerializer
 import uuid
-from pathlib import Path
-from django.core.files import File as DjangoFile
-from django.conf import settings
 import logging
 from typing import TYPE_CHECKING
 
@@ -54,47 +51,6 @@ class FileViewSet(BaseViewSet):
         """
         serializer.save(user=self.request.user)
 
-    # @action(detail=False, methods=["post"], url_path="generate-photostrip")
-    # def generate_photostrip(self, request):
-    #     serializer = GeneratePhotostripSerializer(
-    #         data=request.data,
-    #         context={"request": request},
-    #     )
-    #     serializer.is_valid(raise_exception=True)
-
-    #     template_id = serializer.validated_data["template_id"]
-    #     photos = serializer.validated_data["photos"]
-
-    #     event = serializer.validated_data.get("event")
-    #     session = serializer.validated_data.get("session")
-    #     user = request.user
-
-    #     filename = f"{template_id}_{uuid.uuid4().hex}.png"
-    #     temp_path = Path(settings.MEDIA_ROOT) / "tmp" / filename
-
-    #     # 1️⃣ Generate image
-    #     generate_photostrip(
-    #         template_id=template_id,
-    #         photos=photos,
-    #         output_path=temp_path,
-    #     )
-
-    #     # 2️⃣ Save into File model
-    #     with open(temp_path, "rb") as f:
-    #         File.objects.create(
-    #             event=event,
-    #             session=session,
-    #             user=user,
-    #             file=DjangoFile(f, name=filename),
-    #             type=File.Type.PHOTOSTRIP,
-    #         )
-
-    #     # 3️⃣ Optional: cleanup temp file
-    #     temp_path.unlink(missing_ok=True)
-
-    #     return Response(
-    #         status=status.HTTP_201_CREATED,
-    #     )
     @action(detail=False, methods=["post"], url_path="generate-photostrip")
     def generate_photostrip(self, request):
         serializer = GeneratePhotostripSerializer(
